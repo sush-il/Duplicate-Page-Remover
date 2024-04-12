ca3591a589684306c683d2d2935d94ea47d2a0c9
Sushil <34659821+sush-il@users.noreply.github.com>
2024-04-12 23:10:41 +0100

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from server import views
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
+app.register_blueprint(views, url_prefix="/fileUpload")
+
+#/fileUpload
 
 #for deployment
 if __name__=='__main__':
