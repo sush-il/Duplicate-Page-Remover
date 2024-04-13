e06f72d4545112ce970a2c96df6ca7a95a739ba2
Sushil <34659821+sush-il@users.noreply.github.com>
2024-04-13 08:55:32 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from flask import Flask
 from server import views
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/")
 app.config['SERVER_NAME'] = 'duplicate-page-remover.onrender.com'
+app.register_blueprint(views, url_prefix="/")
 
 #for deployment
 if __name__=='__main__':
